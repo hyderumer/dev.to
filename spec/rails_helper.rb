5567e2057b721a81f4ae83d134a2676ed911a69e
mstruve <mollylbs@gmail.com>
2020-06-03 15:01:21 -0500

    Be Smart with jobs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -164,6 +164,10 @@ RSpec.configure do |config|
     allow(Percy).to receive(:snapshot)
   end
 
+  config.after(:all) do
+    Timecop.return
+  end
+
   OmniAuth.config.test_mode = true
   OmniAuth.config.logger = Rails.logger
 
